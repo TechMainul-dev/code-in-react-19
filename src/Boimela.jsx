78cf7c335825f96c55bcfcceef7fedd9f5f91759
Mainul Hasan Khan <ahnafkhanhibban@outlook.com>
2025-01-28 00:01:24 +0600

    add props and hierarchy

---
+++ src/Boimela.jsx
@@ -0,0 +1,15 @@
+import BookList from './BookList';
+import Header from './Header';
+import Search from './Search';
+
+function Boimela() {
+  return (
+    <div className='mx-auto p-4'>
+      <Header />
+      <Search />
+      <BookList />
+    </div>
+  );
+}
+
+export default Boimela;
